53d36d207c207f99e2eddd5499f4024f5e041a47
wilson mwangi waweru <kouragitam@gmail.com>
2024-06-30 00:05:26 +0300

    adds scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'anyone'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'anyone else';
+}
